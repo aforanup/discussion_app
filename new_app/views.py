91484c549f02cb9b058d3171d8823f09f5195d52
aforanup <aforanupn@gmail.com>
2022-01-13 22:30:34 +0545

    registration added

--- new_app/views.py
+++ new_app/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -77,20 +78,30 @@ def loginUser(request):
 
         try:
             User.objects.get(username=username)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Username and Password doesnot match')
         except:
             messages.error(request, 'Username doesnot exist.')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'Username and Password doesnot match')
     return render(request, 'new_app/login_registration.html', {'page': page})
 
 
 def registerUser(request):
-    page = 'register'
-    return render(request, 'new_app/login_registration.html', {'page': page})
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Couldn't register try again")
+    return render(request, 'new_app/login_registration.html', {'form': form})
 
 
 def logoutUser(request):
